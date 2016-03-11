409cd0f9f3707469bcd204a8eb743ab562adacc2
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-10 21:08:14 -0500

    s

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/addrow',function(req,res,next){
   var context = {};
   mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) " +
 					"VALUES (?, ?, ?, ?, ?)",
-					[req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs],
+					[req.query.name, req.query.reps, req.query.weight, req.query.date.toDateString(), req.query.lbs],
 					function(err, result){
 	
 	if(err){
