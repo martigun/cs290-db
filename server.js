03aff562b4a0a70c85d6d45b443a806106c50835
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-10 21:50:12 -0500

    s

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.get('/addrow',function(req,res,next){
 		return;
 	}
 	
-  
     context.results = "Inserted id " + result.insertId;
     //context.results = "Add was successful!";
 	res.render('home',context);
@@ -55,10 +54,40 @@ app.get('/delete',function(req,res,next){
 	//tell user
     //context.results = "Deleted " + result.changedRows + " rows.";
 	
-	
 	context.results = "Deleted row id= " + [req.query.id];
 	res.render('home',context);
 	
+  });
+});
+
+app.get('/update',function(req,res,next){
+  var context = {};
+  mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+	
+    if(result.length == 1){
+      var curVals = result[0];
+      mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+					[req.query.name || curVals.name, 
+					req.query.reps || curVals.reps, 
+					req.query.weight || curVals.weight, 
+					req.query.date || curVals.date, 
+					req.query.lbs || curVals.lbs, 
+					req.query.id],
+					
+        function(err, result){
+        if(err){
+          next(err);
+          return;
+        }
+		
+        context.results = "Updated " + result.changedRows + " rows.";
+        res.render('home',context);
+      });
+    }
   });
 });
 
