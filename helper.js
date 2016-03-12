c55b5a5746fe103dcdbc06b910f58e9541be6df1
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-11 21:29:17 -0500

    s

--- helper.js
+++ helper.js
@@ -61,7 +61,9 @@ function getTable(Content){
 		};
 	});
 	
-	req.send(null); //keep on truckin, since async=true!
+	//activates when call comes back
+	req.send(null);
+	
 	
 	
 	
@@ -74,45 +76,9 @@ function getTable(Content){
 //on city click
 function addClick(Content){
 	
+	alert("addClick was clicked!");
 	
-	
-	
-	console.log("--- Add click was pushed again ---");
-
-	//console.log("value is:" + document.getElementById("input_name").value);
-	
-	var input_name = document.getElementById("input_name").value;
-	
-	if(!input_name){
-		console.log("value is empty!");
-	} else {
-		
-		console.log("value is:" + input_name);
-	}
-	
-	// //get city input value
-	// var city_input = document.getElementById("city_input").value;
-	
-	// var req = new XMLHttpRequest();
-	// req.open("GET", root + city_input + appid, true); //true for async!
-	
-	// //waits for the response to load
-	// req.addEventListener('load',function(){
-		// //check status
-		// if(req.status >= 200 && req.status < 400){
-			// //parse results for console
-			// console.log(JSON.parse(req.responseText));
-			
-			// //send results string to page
-			// document.getElementById("weatherResults").textContent = req.responseText;
-		// } else {
-			// //there was an error
-			// console.log("Error in network request: " + req.statusText);
-		// };
-	// });
-	
-	// req.send(null); //keep on truckin, since async=true!
 };
 
 //add listener
-//document.getElementById("addButton").addEventListener("click", addClick);
+document.getElementById("addButton").addEventListener("click", addClick);
