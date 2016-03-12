e267d4961dd67c6848ff02c6be6ca3cfeae3bbc6
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-11 22:30:51 -0500

    s

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-var express = require('express');
-var mysql = require('./connectSQL.js');//this??
+//Gunnar Martin
+//CS290
+//Winter 2016
+//Assignment: DB Interaction
 
-var app = express();
+var express = require('express'); //require express
+var mysql = require('./connectSQL.js'); //require my connection info
+
+var app = express(); //initiate express
+
+//initiate handlebars
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+//set port
 app.set('port', 3300);
 
+//use this directory
 app.use(express.static(__dirname));
 
+//route path
 app.get('/',function(req,res,next){
 	
+	//create context object
 	var context = {};
+	
+	//run select query
 	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
 		
 		//set the dArray to the rows object
@@ -22,63 +35,44 @@ app.get('/',function(req,res,next){
 		res.render('form', context);
 	});	
 	
-	console.log(req.originalUrl);
-	
-});
-
-app.get('/empty',function(req,res,next){
-	
+	//print url to console
 	console.log(req.originalUrl);
 });
 
-app.get('/addrow2',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) " +
-					"VALUES (?, ?, ?, ?, ?)",
-					[req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs],
-					function(err, result){
-	
-	if(err){
-		next(err);
-		return;
-	}
-	
-	console.log(req.originalUrl);
-	
-	res.send(req.originalUrl);
-  });
-	
-
-});
-
-
-
+//add row
 app.get('/addrow',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) " +
+	
+	//create context object
+	var context = {};
+	
+	//run insert query
+	mysql.pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) " +
 					"VALUES (?, ?, ?, ?, ?)",
 					[req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs],
 					function(err, result){
 	
+	//check for erros
 	if(err){
 		next(err);
 		return;
 	}
 	
+	//print url to console
 	console.log(req.originalUrl);
 	
-    //context.results = "Inserted id " + result.insertId;
-    //context.results = "Add was successful!";
-	//res.render('home',context);
+	//show root
 	res.redirect("/");
   });
 });
 
-//<input type="hidden" name="id" value="3">
-
+//delete row
 app.get('/delete',function(req,res,next){
-  var context = {};
-  mysql.pool.query("DELETE FROM workouts WHERE id=?",
+	
+	//create context object
+	var context = {};
+	
+	//run delete query
+	mysql.pool.query("DELETE FROM workouts WHERE id=?",
 					[req.query.id],
 					function(err, result){
 	
@@ -88,25 +82,27 @@ app.get('/delete',function(req,res,next){
 		return;
 	}
 	
-	//tell user
+	//print url to console
 	console.log(req.originalUrl);
 	
-	//redirect
+	//show root
 	res.redirect("/");
-	
-	//context.results = "Deleted row id= " + [req.query.id];
-	//res.render('home',context);
-	
   });
 });
 
+//update a record
 app.get('/update',function(req,res,next){
+	
+	//create context object
 	var context = {};
   
-	//trace path
+	//print url to console
 	console.log(req.originalUrl);
-  
+	
+	//run select query on this id
 	mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+		
+		//check for errors
 		if(err){
 		  next(err);
 		  return;
@@ -115,35 +111,40 @@ app.get('/update',function(req,res,next){
 		//if result comes back...
 		if(result.length == 1){
 			
-		//set variable to result
-		var curVals = result[0];
-		
-		//run update query
-		mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
-			[req.query.name || curVals.name, 
-			req.query.reps || curVals.reps, 
-			req.query.weight || curVals.weight, 
-			req.query.date || curVals.date, 
-			req.query.lbs || curVals.lbs, 
-			req.query.id],
+			//set variable to result
+			var curVals = result[0];
 			
-			function(err, result){
-				if(err){
-					next(err);
-					return;
-				}
+			//run update query
+			mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+				[req.query.name || curVals.name, 
+				req.query.reps || curVals.reps, 
+				req.query.weight || curVals.weight, 
+				req.query.date || curVals.date, 
+				req.query.lbs || curVals.lbs, 
+				req.query.id],
 				
-				//print path and redirect to main
-				res.redirect("/");
-			});
+				//check for errors and show root
+				function(err, result){
+					if(err){
+						next(err);
+						return;
+					}
+					
+					//show root
+					res.redirect("/");
+				}
+			);
 		}
   });
 });
 
+//edit a record
 app.get('/edit',function(req,res,next){
 
+	//create context object
 	var context = {};
 	
+	//run select query
 	mysql.pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, rows, fields){
 		
 		//set the dArray to the rows object
@@ -153,15 +154,11 @@ app.get('/edit',function(req,res,next){
 		res.render('update', context);
 	});	
   
-
-  console.log(req.originalUrl);
-  
+	//print url to console
+	console.log(req.originalUrl);
 });
 
-
-
-
-
+//reset table from the instructor
 app.get('/reset-table',function(req,res,next){
   var context = {};
   mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
@@ -181,84 +178,6 @@ app.get('/reset-table',function(req,res,next){
   });
 });
 
-app.get('/get',function(req,res,next){
-	
-	var context = {};
-	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
-		
-		//set the dArray to the rows object
-		context.dArray = rows;
-		
-		//render the context
-		res.json(context);
-	});	
-	
-	//console.log(req.originalUrl);
-});
-
-
-app.get('/render',function(req,res,next){
-	
-	var context = {};
-	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
-		
-		//set the dArray to the rows object
-		context.dArray = rows;
-		
-		//render the context
-		res.render('form2', context);
-	});	
-	
-	console.log(req.originalUrl);
-});
-
-app.get('/json', 
-	function(req,res,next){
-	
-	var context = {};
-	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
-		
-		context.results = JSON.stringify(rows);
-		
-		res.render('home', context);
-		
-			//get field names
-		for (var i=0; i < fields.length ; i++) console.log(i + ": " + fields[i].name);
-	
-
-	});	
-	
-		
-});
-
-app.get('/data',function(req,res,next){
-	
-	var context = {};
-	mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
-		
-		//set the dArray to the rows object
-		context.dArray = rows;
-		
-		//render the context
-		res.render('data', context);
-	});	
-});
-
-app.get('/insert',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.c], function(err, result){
-	
-	if(err){
-		next(err);
-		return;
-	}
-	
-  
-    context.results = "Inserted id " + result.insertId;
-    //context.results = "Add was successful!";
-	res.render('home',context);
-  });
-});
-
+//run on 3300
 console.log("Running on 3300...")
 app.listen(3300);
