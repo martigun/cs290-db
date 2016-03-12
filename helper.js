accb8d28678a73755b2a34fe264eee8feaa04125
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-11 20:32:49 -0500

    s

--- helper.js
+++ helper.js
@@ -32,6 +32,28 @@ function getTable(Content){
 	//console.log("value is:" + document.getElementById("input_name").value);
 	
 	
+	var req = new XMLHttpRequest();
+	req.open("GET", "/get", true); //true for async!
+	
+	//waits for the response to load
+	req.addEventListener('load',function(){
+		//check status
+		if(req.status >= 200 && req.status < 400){
+			//parse results for console
+			console.log(JSON.parse(req.responseText));
+			
+			document.getElementById("testData").textContent = req.responseText;
+			
+			//send results string to page
+			//document.getElementById("weatherResults").textContent = req.responseText;
+		} else {
+			//there was an error
+			console.log("Error in network request: " + req.statusText);
+		};
+	});
+	
+	req.send(null); //keep on truckin, since async=true!
+	
 	
 	
 
