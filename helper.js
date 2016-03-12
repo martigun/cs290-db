a9cd07a634a4eeb2086b76439bb0119492c4f334
martigun@onid.oregonstate.edu <martigun@onid.oregonstate.edu>
2016-03-11 22:03:06 -0500

    s

--- helper.js
+++ helper.js
@@ -21,45 +21,47 @@ function getTable(Content){
 		//check status
 		if(req.status >= 200 && req.status < 400){
 			
-			//get data table
-			var dataTable = document.getElementById("dataTable");
-			
 			//move results to dArray
 			var obj = JSON.parse(req.responseText);			
 			var dArray = obj.dArray;
+			
+			displayTable(dArray);
+			
+			// //get data table
+			// var dataTable = document.getElementById("dataTable");
 						
-			//Build table from this array
-			for(var i=0; i < dArray.length; i++){
+			// //Build table from this array
+			// for(var i=0; i < dArray.length; i++){
 				
-				//create row
-				var thisRow = document.createElement("tr");
+				// //create row
+				// var thisRow = document.createElement("tr");
 				
-				var tFields = ["name","reps","weight","date","lbs"];
-				for(var j=0; j < tFields.length; j++){
+				// var tFields = ["name","reps","weight","date","lbs"];
+				// for(var j=0; j < tFields.length; j++){
 					
-					var data_cell = document.createElement("td");
-					var thisData = dArray[i][tFields[j]];
+					// var data_cell = document.createElement("td");
+					// var thisData = dArray[i][tFields[j]];
 					
-					//format dates
-					if(tFields[j]=="date") {
-						var date = new Date(thisData);
-						thisData = date.toDateString();
-					}
+					// //format dates
+					// if(tFields[j]=="date") {
+						// var date = new Date(thisData);
+						// thisData = date.toDateString();
+					// }
 					
-					data_cell.textContent = thisData;
+					// data_cell.textContent = thisData;
 					
-					thisRow.appendChild(data_cell);
-				}
-				 //Add delete button
-				var deleteCell = document.createElement("td");
-				var deleteButton = document.createElement("button");
-				deleteButton.textContent = "Delete";
-				deleteCell.appendChild(deleteButton);
-				thisRow.appendChild(deleteCell);
+					// thisRow.appendChild(data_cell);
+				// }
+				 // //Add delete button
+				// var deleteCell = document.createElement("td");
+				// var deleteButton = document.createElement("button");
+				// deleteButton.textContent = "Delete";
+				// deleteCell.appendChild(deleteButton);
+				// thisRow.appendChild(deleteCell);
 				
-				//append row to table
-				dataTable.appendChild(thisRow);
-			}
+				// //append row to table
+				// dataTable.appendChild(thisRow);
+			// }
 			
 		} else {
 			//there was an error
@@ -78,6 +80,50 @@ function getTable(Content){
 	
 	
 };
+
+function displayTable(dArray){
+	
+	//get data table
+	var dataTable = document.getElementById("dataTable");
+	
+	//Build table from this array
+	for(var i=0; i < dArray.length; i++){
+		
+		//create row
+		var thisRow = document.createElement("tr");
+		
+		var tFields = ["name","reps","weight","date","lbs"];
+		for(var j=0; j < tFields.length; j++){
+			
+			var data_cell = document.createElement("td");
+			var thisData = dArray[i][tFields[j]];
+			
+			//format dates
+			if(tFields[j]=="date") {
+				var date = new Date(thisData);
+				thisData = date.toDateString();
+			}
+			
+			data_cell.textContent = thisData;
+			
+			thisRow.appendChild(data_cell);
+		}
+		 //Add delete button
+		var deleteCell = document.createElement("td");
+		var deleteButton = document.createElement("button");
+		deleteButton.textContent = "Delete";
+		deleteCell.appendChild(deleteButton);
+		thisRow.appendChild(deleteCell);
+		
+		//append row to table
+		dataTable.appendChild(thisRow);
+	}
+	
+	
+	
+	
+	
+}
 
 
 //on city click
@@ -114,8 +160,27 @@ function addClick(Content){
 	
 	alert(qString);
 	
+	var req = new XMLHttpRequest();
+	req.open("GET", qString, true); //true for async!
 	
+	//waits for the response to load
+	req.addEventListener('load',function(){
+		//check status
+		if(req.status >= 200 && req.status < 400){
+			
+			console.log("success!");
+
+			
+			
+		} else {
+			
+			//there was an error
+			console.log("Error in network request: " + req.statusText);
+		};
+	});
 	
+	//activates when call comes back
+	req.send(null);
 	
 	
 	
